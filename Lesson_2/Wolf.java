b1c911e9077e477626fad471ec8e9f18f17b1e55
MakismDurg <117468144+MakismDurg@users.noreply.github.com>
2022-12-24 04:37:30 +0300

    Волк. Начало.

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,32 @@
+public class Wolf {
+	String name;
+	String gender;
+	int age;
+	double weight;
+	String colour;
+
+	void go() {
+		System.out.println("Он ходит");
+	}
+
+	void run() {
+		System.out.println("Он бегает");
+	}
+
+	void sit() {
+		System.out.println("Он сидит");
+	}
+
+	void howl() {
+		System.out.println("Он воет");
+	}
+
+	void hunt() {
+		System.out.println("Он охотится");
+	}
+
+	boolean doesHePerformAtCircus() {
+		System.out.println("Волк слабее льва и тигра, но в цирке не выступает.");
+		return true;
+	}
+}
