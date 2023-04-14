4ea1fa49190cbd0f15ce1477c95ee32f458d2536
makism_durg <maksim_pavlov_1993@inbox.ru>
2023-04-14 19:56:22 +0300

    Задание со * второй редакции

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,23 +1,23 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    public static double calculate(String mathExpression) throws IllegalStateException {
-        String[] splittedExpression = mathExpression.split(" ");
-        double a = Integer.parseInt(splittedExpression[0]);
-        char sign = splittedExpression[1].charAt(0);
-        double b = Integer.parseInt(splittedExpression[2]);
-        if (a < 0 | b < 0){
+    public static double calculate(String mathExpression) {
+        String[] elements = mathExpression.split(" ");
+        double a = Integer.parseInt(elements[0]);
+        char sign = elements[1].charAt(0);
+        double b = Integer.parseInt(elements[2]);
+        if (a <= 0 || b <= 0) {
             throw new IllegalStateException("Числа должны быть положительными");
         }
 
         return switch (sign) {
-            case '+' ->  a + b;
-            case '-' ->  a - b;
-            case '*' ->  a * b;
-            case '/' ->  a / b;
-            case '^' ->  Math.pow(a, b);
-            case '%' ->  a % b;
-            default -> throw new IllegalStateException("Знак вычислния введён неверно. Используй +, -, *, /, ^, %");
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> a / b;
+            case '^' -> Math.pow(a, b);
+            case '%' -> a % b;
+            default -> throw new IllegalStateException("Знак вычисления введён неверно. Используй +, -, *, /, ^, %");
         };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,8 +2,6 @@ package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
-import static com.startjava.lesson_2_3_4.calculator.Calculator.calculate;
-
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
@@ -14,23 +12,23 @@ public class CalculatorTest {
                 if (option.equals("yes")) {
                     System.out.print("Введите математическое выражение: ");
                     String mathExpression = console.nextLine();
-                    double result = calculate(mathExpression);
-                    outputResult(result);
+                    double result = Calculator.calculate(mathExpression);
+                    print(result);
                 } else {
                     System.out.println("Вариантов ответа только два");
                 }
                 System.out.println("Хотите продолжить вычисления? [yes/no]");
                 option = console.nextLine();
-            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                System.out.println("Требуется вводить целые числа и знак вычисления через пробел");
             } catch (IllegalStateException e) {
                 System.out.println(e.getMessage());
+            } catch (RuntimeException e) {
+                System.out.println("Требуется вводить целые числа и знак вычисления через пробел");
             }
         } while (!option.equals("no"));
         System.out.println("Расчёты завершены.");
     }
 
-    public static void outputResult(double result) {
+    public static void print(double result) {
         System.out.print("Результат вычислений = ");
         System.out.printf((result % 1 != 0 ? "%.3f" : "%.0f") + "\n", result);
     }
